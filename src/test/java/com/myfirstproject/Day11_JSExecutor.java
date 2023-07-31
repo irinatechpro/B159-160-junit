25e3353b52ae16af00b707cd634b71602d7c27ac
Iryna R <mnops@ukr.net>
2023-07-31 13:02:39 +0100

    day 11

--- src/test/java/com/myfirstproject/Day11_JSExecutor.java
+++ src/test/java/com/myfirstproject/Day11_JSExecutor.java
@@ -5,6 +5,7 @@ import org.checkerframework.checker.units.qual.A;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
@@ -12,17 +13,14 @@ public class Day11_JSExecutor extends TestBase {
 
     @Test
     public void jsExecutorTest1() throws InterruptedException {
-
 //        Given user is on the https://www.amazon.com/
         driver.get("https://www.amazon.com/");
         Thread.sleep(2000);
 //        When use click on “Account” link
         Actions actions = new Actions(driver);
-        WebElement accountList = driver.findElement(By.id("nav-link-accountList"));//ALTERNATIVELY
+       // WebElement accountList = driver.findElement(By.id("nav-link-accountList"));
+        WebElement accountList = locateElementByJS("nav-link-accountList");
         actions.moveToElement(accountList).perform();
-
-        //WebElement accountList =locateElementByJS("nav-link-accountList");
-
         Thread.sleep(2000);
         WebElement accountLink = driver.findElement(By.linkText("Account"));
 //        accountLink.click(); ALTERNATIVELY ( ESPECIALLY WHEN NORMAL CLICK DOESN'T WORK)
@@ -35,24 +33,24 @@ public class Day11_JSExecutor extends TestBase {
 //       actions.moveToElement(amazonMusic).perform(); // ALTERNATIVELY
        scrollIntoViewJS(amazonMusic);
        Thread.sleep(2000);
-////        And click on it
-////        amazonMusic.click();//ALTERNATIVELY
+//        And click on it
+//        amazonMusic.click();//ALTERNATIVELY
         clickByJS(amazonMusic);
         Thread.sleep(2000);
-////        Then verify Amazon Music page is displayed
+//        Then verify Amazon Music page is displayed
         Assert.assertTrue(driver.getCurrentUrl().contains("music"));
-////        Scroll the page up and down
+//        Scroll the page up and down
         scrollAllDownJS();
         Thread.sleep(2000);
         scrollAllUpJS();
         Thread.sleep(2000);
-//
-////        search for Pop
-//        WebElement searchBox = locateElementByJS("navbarSearchInput");
-//        setValueByJS(searchBox,"Pop");
-//
-//        Thread.sleep(2000);
-//        System.out.println(getValueByJS("navbarSearchInput"));//getting the value of that search box
+
+//        search for Pop music
+        WebElement searchBox = locateElementByJS("navbarSearchInput");
+        setValueByJS(searchBox,"Pop");
+
+        Thread.sleep(2000);
+        System.out.println(getValueByJS("navbarSearchInput"));//getting the value of that search box
     }
 }
 
